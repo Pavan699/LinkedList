4f1e812ed1e0f0cc108f70e89bb0b8de371966fa
Pavan699 <pavannakate03@gmail.com>
2021-09-21 23:55:27 -0700

    Simple Linked List declaration

--- LinkedListProgram/Program.cs
+++ LinkedListProgram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LinkedListProgram
 {
@@ -7,6 +8,15 @@ namespace LinkedListProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Linked List Program Solution");
+            LinkedList<int> llist = new LinkedList<int>();//declaration of linked list
+            llist.AddFirst(56);//inbuild methds(Addfirst,AddLast,)
+            llist.AddLast(30);
+            llist.AddLast(70);
+            Console.WriteLine("Printing Normal Linked List : 56,30,70");
+            foreach(var i in llist)//printing the Linked List
+            {
+                Console.WriteLine("Element is : "+i);
+            }
         }
     }
 }
