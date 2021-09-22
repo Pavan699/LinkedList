40090eb90cb0d27d5a0e93b24769942595d265f2
Pavan699 <pavannakate03@gmail.com>
2021-09-22 02:11:25 -0700

    Update Insert method and add switch-case

--- LinkedListProgram/Choice.cs
+++ LinkedListProgram/Choice.cs
@@ -9,38 +9,81 @@ namespace LinkedListProgram
         public void Input()
         {
             LinkedList<int> llist = new LinkedList<int>();//declaration of linked list
-            llist.Addfirst(70);
-            llist.Addfirst(30);
-            llist.Addfirst(56);
-            llist.Print();
 
-            /*llist.Append(56);//calling Append method
-            llist.Append(30);
-            llist.Append(70);
-            llist.Print();*/
-
-            /*llist.Addfirst(70);            
-            llist.Addfirst(56);
-            llist.Insert(2, 30);
-            llist.Print();*/
-
-            /*llist.DeleteFirst();
-            llist.Print();*/
-
-            /*llist.DeleteLast();
-            llist.Print();*/
-
-            bool check = llist.Search(30);
-            if(check == true)
+            int count = 0, defCount = 1;
+            while (defCount == 1)
             {
-                Console.WriteLine("Element {0} is Present",30);
+                Console.Write("Enter The Input : \n 1.Add First \t2.Add Last  \t 3.Insert \n 4.Delete First\t5.Delete Last\t 6.Search Element \n 7.Display \t8.Exit \n");
+                Console.Write("Input Plz : ");               
+                int check = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                switch (check)
+                {
+                    case 1:
+                        Console.Write("Enter How Many Elements To add :");
+                        int numfirstadd = Convert.ToInt32(Console.ReadLine());
+                        for(int i = 0; i< numfirstadd; i++)
+                        {
+                            Console.Write("Enter Element To add at first:");
+                            int element1 = Convert.ToInt32(Console.ReadLine());
+                            llist.Addfirst(element1);
+                        }
+                        count++;
+                        break;
+                    case 2:
+                        Console.Write("Enter How Many Elements To add :");
+                        int numlastadd = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < numlastadd; i++)
+                        {
+                            Console.Write("Enter Element To add at last:");
+                            int element2 = Convert.ToInt32(Console.ReadLine());
+                            llist.Append(element2);
+                        }
+                        count++;
+                        break;
+                    case 3:
+                        llist.Print();
+                        Console.Write("Enter the Index : ");
+                        int Index = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Element To add :");
+                        int value = Convert.ToInt32(Console.ReadLine());
+                        llist.Insert(Index+1, value);
+                        count++;
+                        break;
+                    case 4:
+                        llist.DeleteFirst();
+                        count++;
+                        break;
+                    case 5:
+                        llist.DeleteLast();
+                        count++;
+                        break;
+                    case 6:
+                        Console.Write("Enter Element To check it is Present or Absent : ");
+                        int element3 = Convert.ToInt32(Console.ReadLine());
+                        bool Present = llist.Search(element3);
+                        if (Present == true)
+                        {
+                            Console.WriteLine("Element {0} is Present", element3);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Element {0} is Absent", element3);
+                        }
+                        count++;
+                        break;
+                    case 7:
+                        llist.Print();
+                        count++;
+                        break;
+                    default:
+                        Console.WriteLine("Thank You.....!");
+                        defCount++;
+                        break;
+                }
             }
-            else
-            {
-                Console.WriteLine("Element {0} is Absent", 30);
-            }
-
+            Console.WriteLine("Counter of the Operations Done is : " + count);
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         }
-
     }
 }
